199ab539323f6173aacfac9f753c954d5fa08fb5
josevictormoura <josevictormouraa@gmail.com>
2025-01-17 14:43:13 -0300

    Adicionado config de estilos personalizados e tipografia

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,24 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    fontFamily:{
+      rubik: ["Rubik" ,"seif"]
+    },
+    container:{
+      center: true,
+      padding:{
+        default: '.5rem',
+        sm:'1rem'
+      }
+    },
+    extend: {
+      colors: {
+        "soft-blue": "hsl(231, 69%, 60%)",
+        "soft-red": "hsl(0, 94%, 66%)",
+        "grayish-blue": "hsl(229, 8%, 60%)",
+        "Very-dark-blue": "hsl(229, 31%, 21%)"
+      }
+    },
   },
   plugins: [],
 }
